f5842d71fefb7e99675b13ca3909e175def2cbae
AdrianCzebiniak <Adrian@artivest.co>
2020-03-18 19:06:01 -0400

    Update clock.js

--- clock.js
+++ clock.js
@@ -4,13 +4,14 @@ function pad(str, max) {
 }
 
 function currentTime() {
+  console.log('here');
   var dt = new Date();
   var ampm = dt.getHours() > 12 ? 'PM' : 'AM';
   var hours = dt.getHours() % 12
   hours = hours ? hours : 12;
   
   var time = pad(hours, 2) + ":" + pad(dt.getMinutes(), 2) + ":" + pad(dt.getSeconds(), 2) + " " + ampm;
-  $('#clock').innerText = time;
+  $('#clock').text(time);
 
   var t = setTimeout(currentTime, 1000);
 }
